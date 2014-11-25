03df45bee36f128db10eb174a742bbf8340411e8
yinchuandong <yincd520@sina.com>
2014-11-25 22:48:52 +0800

    让她

---
+++ js/security.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wjw on 2014/11/25.
+ */
+
+(function (){
+    var security ={
+        init:function(){
+
+        },
+        bindEvent:function(){
+            
+        }
+
+    }
+    security.init();
+})()
