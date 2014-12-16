53919b48acbf9e30f93d7219bcdf5c668b05d9d1
minhao1021 <397836525@qq.com>
2014-12-16 12:26:21 +0800

    asdf

--- js/shouldknow.js
+++ js/shouldknow.js
@@ -15,13 +15,30 @@
 			var navH = $(".j-mainl").offset().top-50;
             $(window).scroll(function(){
                 var scroH = $(this).scrollTop();
+                var windowH = $(window).height();
+                var bodyH = $(document).height();
+                var $title = $('#mainr .title');
+                var $label = $('#mainl li').not(":first");
+                var h;//导航距离底部高度
+                var n=0;//所在版块下标
                 if(scroH>=navH){
                     $(".j-mainl").css({"position":"fixed","top":"50px"});
                 }
                 else if(scroH<navH){
                     $(".j-mainl").css({"position":"static"});
                 }
-            })
+                h = bodyH-scroH-windowH-270
+                if(h<0){
+                	$(".j-mainl").css({"top":(50+h)+"px"});
+                }
+                for(i=0;i<$title.length;i++){
+                	if($($title[i]).offset().top-scroH-180<0){
+                		n = i;
+                	}
+                }
+                $label.removeClass("blue");
+                $($label[n]).attr({"class":"blue"});
+            });
 		}
 	}
 	Should.init();
